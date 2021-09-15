94c5c0334f2292b4b13fdebbe779bf97e6207c02
Freida Anne Blostein <blostein@umich.edu>
2021-09-15 17:08:39 -0400

    'added surrogate variables (complete matrix) to SVA Report and to models in script 03'

--- Scripts/03_summary_methyl_models.R
+++ Scripts/03_summary_methyl_models.R
@@ -42,6 +42,11 @@ modeldata$SmkAtVisitPastmonth<-factor(modeldata$SmkAtVisitPastmonth,
                                                "Pack or more a day", "Missing"))
 modeldata$slide<-factor(modeldata$slide)
 modeldata$Sample_Plate<-factor(modeldata$Sample_Plate)
+#sv data - may change this depending on conversation w/ kelly
+load(paste0(datadir, 'svNone.Rds'))
+is_sv=function(x) str_replace_all(x, " ", "")
+sv.vNone=sv.vNone%>%rename_with(is_sv, contains('sv'))
+modeldata=left_join(modeldata, sv.vNone)
 
 #################################Set variables for modeling
 #outcomes & outcome_labels
@@ -49,7 +54,8 @@ y_vector<-c("globalmethylation", "pediatric", "grim", "anynewborn_center", "SSne
 outcome_labels=c('Global methylation', 'Pediatric clock', 'GRIM clock',  
                  'Any smoking polymethylation score (newborns)', 
                  'Sustained smoking polymethylation score (newborns, cell-type controlled)', 
-                 'Sutained smoking polymethylation score (older children)')
+                 'Sustained smoking polymethylation score (older children)')
+names(outcome_labels)=y_vector
 
 #model variables (excepting PCs, add in individually for global & ancestry specific models)
 base_model_vars<-"~smkPreg_binary+cm1bsex+cm1inpov+ChildAgeComposite+Epi+IC+Sample_Plate"
@@ -59,18 +65,23 @@ secondhand_model_vars<-paste0(prenatal_model_vars, '+PostnatalMaternalSmokingAny
 global_pcs='+global_PC1+global_PC2'
 local_pcs='+local_PC1+local_PC2'
 
+#surrogate variable models
+surrogate_model_vars<-paste0('~smkPreg_binary+', 
+                             paste(colnames(modeldata)[str_detect(colnames(modeldata), 'sv')], collapse='+'))
+
 predictors=c('base model'=base_model_vars, 
              'prenatal exposures model'=prenatal_model_vars, 
              'secondhand smoke exposure model'=secondhand_model_vars)
-model_labels=c('base model', 'prenatal exposures model', 'secondhand smoke exposure model')
 
-#surrogate variable models
-surrogate_model_vars<-""
+model_labels=c('base model', 'prenatal exposures model', 'secondhand smoke exposure model', 'surrogate variable models')
+
+
 
 #stratification variables 
 ##############################
-age_vector=modeldata$childteen%>%unique()
 age_labels=c('Age 9', 'Age 15')
+age_vector=modeldata$childteen%>%unique()
+names(age_labels)=age_vector
 ancestry_vector=modeldata$ancestry%>%unique()
 
 #################################Source functions
@@ -87,10 +98,22 @@ model_lmm<-function(df, y, x){
   lmer(formula(paste(y, xnew)), data=df, REML=F)
 }
 
+my_clean_argnames=function(df, my_preds, childteen=T){
+  names(model_labels)=my_preds
+  df=df %>% mutate(predictors=recode_factor(predictors, !!!model_labels),
+                outcome=recode_factor(outcome, !!!outcome_labels))
+  if(childteen==T){df=df%>%mutate(age=recode_factor(childteen, !!!age_labels))} 
+  return(df)
+}
+
+my_forest_plot<-function(df, vars){
+  df %>% ggplot(aes(x=estimate, xmin=conf.low, xmax=conf.high, color=childteen))
+}
+  
 #################################Cross sectional models
 #########################Global
 #set predictor variables & arguments
-global_predictors=paste0(predictors, global_pcs)
+global_predictors=c(paste0(predictors, global_pcs), surrogate_model_vars)
 args=list('childteen'=age_vector, 'outcome'=y_vector, 'predictors'=global_predictors)%>%cross_df()
 
 #run models
@@ -100,9 +123,7 @@ global_models=modeldata%>%group_by(childteen)%>%nest()%>%left_join(args)%>%
          glanced=map(model, glance))
 
 #clean argument names for plotting
-global_models=global_models%>%mutate(predictors=factor(predictors, labels=model_labels), 
-                       outcome=factor(outcome, labels=outcome_labels), 
-                       age=factor(childteen, levels=c('C', 'T', labels=age_labels)))
+global_models=my_clean_argnames(global_models, global_predictors)
 
 #########################Local
 #arguments
@@ -111,14 +132,12 @@ args=list('childteen'=c('C', 'T'), 'outcome'=y_vector, 'predictors'=local_predic
 
 #run models
 local_models=modeldata%>%group_by(childteen, ancestry)%>%nest()%>%left_join(args)%>%
-  mutate(model=pmap(list(data, outcome, predictors), model), 
+  mutate(model=pmap(list(data, outcome, local_predictors), model), 
          tidied=map(model, tidy, conf.int=T), 
          glanced=map(model, glance))
 
 #clean argument names for plotting
-local_models=local_models%>%mutate(predictors=factor(predictors, labels=model_labels), 
-                                     outcome=factor(outcome, labels=outcome_labels), 
-                                     age=factor(childteen, levels=c('C', 'T', labels=age_labels)))
+local_models=my_clean_argnames(local_models, local_predictors)
 
 #################################Longitudinal models
 #########################Global
@@ -127,7 +146,26 @@ global_predictors=paste0(predictors, global_pcs, '+ChildAgeComposite')
 args=list('outcome'=y_vector, 'predictors'=global_predictors)%>%cross_df()
 
 #run models
-longitduinal_global_models=modeldata%>%droplevels()%>%
-  mutate(model=pmap(list(data, outcome, predictors), model), 
+longitudinal_global_models=args %>% 
+    mutate(model=pmap(list(y=outcome, x=predictors), model_lmm, df=modeldata), 
+           tidied=map(model, tidy, conf.int=T), 
+           glanced=map(model, glance))
+
+#clean argument names
+longitudinal_global_models=my_clean_argnames(longitudinal_global_models, global_predictors, childteen = F)
+#########################Local
+#set predictor variables and arguments
+local_predictors=paste0(predictors, local_pcs, '+ChildAgeComposite')
+args=list('outcome'=y_vector, 'predictors'=local_predictors, 'ancestry'=ancestry_vector)%>%cross_df()
+
+#run models
+longitudinal_local_models=modeldata%>%group_by(ancestry)%>%nest()%>%left_join(args)%>%
+  mutate(model=pmap(list(data, outcome, predictors), model_lmm), 
          tidied=map(model, tidy, conf.int=T), 
          glanced=map(model, glance))
+
+#clean argument names
+longitudinal_local_models=my_clean_argnames(longitudinal_local_models, local_predictors, childteen = F)
+
+
+
